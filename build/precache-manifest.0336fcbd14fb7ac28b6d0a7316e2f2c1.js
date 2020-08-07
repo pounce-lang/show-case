e5c66f801234f94272f0188a635e37f79e930d6a
nmorse <morse.nate@gmail.com>
2020-08-07 13:40:15 -0400

    path fix

---
+++ build/precache-manifest.0336fcbd14fb7ac28b6d0a7316e2f2c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e53166ed015e221533d9cbb7e9009a9e",
+    "url": "/show-case/build/index.html"
+  },
+  {
+    "revision": "cc719a1cda046d9a5727",
+    "url": "/show-case/build/static/css/main.31b46580.chunk.css"
+  },
+  {
+    "revision": "ee1cebf11ce1c190203f",
+    "url": "/show-case/build/static/js/2.a3900588.chunk.js"
+  },
+  {
+    "revision": "8ccedb438a2e718e7be481d994a919bf",
+    "url": "/show-case/build/static/js/2.a3900588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc719a1cda046d9a5727",
+    "url": "/show-case/build/static/js/main.97a45096.chunk.js"
+  },
+  {
+    "revision": "3ef114c4d874a4978a22",
+    "url": "/show-case/build/static/js/runtime-main.cc5d4620.js"
+  }
+]);
