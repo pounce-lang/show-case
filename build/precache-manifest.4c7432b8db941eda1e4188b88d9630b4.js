1e7340b3397038080c1de6e648b62cb5211c3cca
nmorse <morse.nate@gmail.com>
2020-11-27 16:28:52 -0500

    after build

---
+++ build/precache-manifest.4c7432b8db941eda1e4188b88d9630b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e881caba58b27c01e9242ca652c85fa4",
+    "url": "/show-case/build/index.html"
+  },
+  {
+    "revision": "800f1b53ad7f595a904b",
+    "url": "/show-case/build/static/css/main.31b46580.chunk.css"
+  },
+  {
+    "revision": "1cc4cc67ac89a476b1e9",
+    "url": "/show-case/build/static/js/2.a2e375e3.chunk.js"
+  },
+  {
+    "revision": "3b61c6e6bb31a3dc9a4e7268e884615d",
+    "url": "/show-case/build/static/js/2.a2e375e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800f1b53ad7f595a904b",
+    "url": "/show-case/build/static/js/main.4f74614f.chunk.js"
+  },
+  {
+    "revision": "de7444a03ea4cec345b7",
+    "url": "/show-case/build/static/js/runtime-main.60085d70.js"
+  }
+]);
