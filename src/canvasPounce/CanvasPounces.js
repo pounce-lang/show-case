88a9c06dfda5f916f265db70bbf5feb393017c52
nmorse <morse.nate@gmail.com>
2022-01-23 21:40:01 -0500

    static aseroids

--- src/canvasPounce/CanvasPounces.js
+++ src/canvasPounce/CanvasPounces.js
@@ -4,12 +4,15 @@ import { useState } from 'react';
 
 const CanvasPounces = () => {
 
-  const [src, setSrc] = useState("[[48 50][40 40][66 50][40 60]]");
+  const [src, setSrc] = useState(`# [[x-trans y-trans rotation-deg][pt-x pt-y][pt-x pt-y]...]
+[[370 200 200][48 50][40 40][66 50][40 60]]
+[[500 200 130][8 3][0 40][66 100][100 60][90 30][100 10]]
+[[100 100 0][8 3][0 70][56 100][56 80][100 40][40 30][50 10]]`);
   return (
     <div className="Feature-parent">
       <div className="Feature-child double-wide" >
         <h3>Asteroids</h3>
-        <p> <textarea
+        <p> <textarea rows="4"
     className="pounceCat-input"
      wrap="true" value={src} 
      onChange={(e)=>setSrc(e.target.value)} 
--- src/canvasPounce/useCanvas.js
+++ src/canvasPounce/useCanvas.js
@@ -2,16 +2,22 @@ import { useEffect, useRef } from 'react';
 
 
 export function draw(ctx, element) {
-  if (!element || element.length < 1 || element[0].length < 2) {
+  if (!element || element.length < 1 || element[0].length < 3) {
     return;
   }
+  ctx.save();
+  ctx.translate(element[0][0], element[0][1]);
+  // ctx.translate(50, 50);
+  ctx.rotate(element[0][2] * Math.PI / 180);
+  ctx.translate(-50, -50);
   ctx.beginPath();
-  ctx.moveTo(element[0][0], element[0][1]);
-  for(let i = 1; i < element.length; i++) {
+  ctx.moveTo(element[1][0], element[1][1]);
+  for(let i = 2; i < element.length; i++) {
     ctx.lineTo(element[i][0], element[i][1]);
   }
   ctx.closePath();
   ctx.stroke();
+  ctx.restore();
   //     ctx.save();
       
   //     //ctx.scale(0.1, 0.1);
