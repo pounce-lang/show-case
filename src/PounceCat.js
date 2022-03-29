cac63fd97a238778a80f5514c1e475b96ef6f53b
nmorse <morse.nate@gmail.com>
2022-03-28 21:23:39 -0400

    demo of list ops

--- src/PounceCat.js
+++ src/PounceCat.js
@@ -15,9 +15,10 @@ const PounceCat = (props) => {
       <datalist id={dlId}>
         <option value={props.pounceCode} label="(original code example)" ></option>
       </datalist>
-      <br/>
+
       yields:<br/>  <div  style={{width:'95.5%', backgroundColor: "rgb(229 206 39)", padding: "2px 2px 2px 8px"}}><code className="stack" >{unParse(result.value.stack)}</code>
       </div>
+      <br/>
     </div>
   );
 };
--- src/coreWords/CorePounces.js
+++ src/coreWords/CorePounces.js
@@ -26,10 +26,12 @@ export const CorePounces = () => {
       <div className="Feature-child double-wide" >
         <h3>[Flow] crouch</h3>
         <p> The word <code>crouch</code> copies named stack items into a phrase. </p>
-        <p> Crouch consumes stack elements and copies those values in. This example, the values for 'm' and 'b' are from the stack and the resulting phrase is funciton for a specific line, to be combined with some 'x' value on the stack later.</p>
+        <p> Crouch consumes stack elements and copies them, by name. 
+          For example, <code>m</code> and <code>b[List Operation] </code> are names, to be associated with stack values <code>4</code> and <code>-1</code>. For each name, a value is popped off of the stack and any name in the phrase is replaced with the value. 
+          The resulting phrase can be used as a function of x for a specific line. Later provided with an 'x' value on the stack, the phrase can be run.</p>
         <PounceCat pounceCode={`# y = mx + b
 4 -1 [m b] [m * b +] crouch`} ></PounceCat>
-<p> Or this phrase could be mapped onto a list of 'x' values, yielding a list of 'y' values</p>
+<p> Or if you had an array of 'x' values, this phrase could be mapped, yielding an array of coresponding 'y' values.</p>
         <PounceCat pounceCode={`[1 2 3] [4 * -1 +] map`} ></PounceCat>
       </div>
       <div className="Feature-child" >
@@ -49,7 +51,24 @@ export const CorePounces = () => {
         <p> <code>word</code> looks up a word signature</p>
         <PounceCat pounceCode="[dup] word" ></PounceCat>
       </div>
-      
+      <div className="Feature-child" >
+        <h3>[List Operation] cons uncons push pop</h3>
+        <p> </p>
+        <PounceCat pounceCode="u [ f v r x o s] cons" ></PounceCat>
+        <PounceCat pounceCode="[ u f v r x o s] uncons" ></PounceCat>
+        <PounceCat pounceCode="[ u f v r x o] s push" ></PounceCat>
+        <PounceCat pounceCode="[ u f v r x o s] pop" ></PounceCat>
+      </div>
+      <div className="Feature-child" >
+        <h3>[List Operation] concat</h3>
+        <p> </p>
+        <PounceCat pounceCode="[u f v] [r x o s] concat" ></PounceCat>
+      </div>
+      <div className="Feature-child" >
+        <h3>[List Operation] split</h3>
+        <p> featured in quick sort, <code>split</code> cuts an array in two. Here <code>t</code> is the value that distinguishes which elements of <code>[ u f v r x o s]</code> are placed in the first or second result, as compared by <code>[>]</code></p>
+        <PounceCat pounceCode="t [ u f v r x o s] [>] split" ></PounceCat>
+      </div>       
     </div>
   );
 };
